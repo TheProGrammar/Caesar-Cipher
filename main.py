e7c3e518761a1ed681fb9709c6c622e47e577c65
Miroslav Mali <68731924+TheProGrammar@users.noreply.github.com>
2022-04-04 21:46:51 -0700

    Update on minor changes
    - Add data py file to contain all extra data needed for the program
    - Add game logo ascii text string for the game
    - Fix the problem of indices going over  the letter_list length when decrypting

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
 import string
+from data import game_logo
 
 # TODO:
 
 letter_list = []
 for i in range(26):
     letter_list.append(string.ascii_lowercase[i])
-for i in range(26):
-    letter_list.append(string.ascii_lowercase[i])
 
-#print(letter_list)
+print(game_logo)
 
-print("""
-                                             _         _                 
-                                            (_)       | |                
-  ___   __ _   ___  ___   __ _  _ __    ___  _  _ __  | |__    ___  _ __ 
- / __| / _` | / _ \/ __| / _` || '__|  / __|| || '_ \ | '_ \  / _ \| '__|
-| (__ | (_| ||  __/\__ \| (_| || |    | (__ | || |_) || | | ||  __/| |   
- \___| \__,_| \___||___/ \__,_||_|     \___||_|| .__/ |_| |_| \___||_|   
-                                               | |                       
-                                               |_|                       
-""")
-
-type_choice = input("Do you want to encrypt or decrypt? Type 'e' or 'd': ")
+type_choice = input("Do you want to encrypt or decrypt a message? Type 'e' or 'd': ")
 
 user_word = input("Enter a word: ")
 password_shift = int(input("Enter a shift amount: "))
@@ -34,6 +22,8 @@ if type_choice == "d":
 for letter in user_word:
     letter_index = letter_list.index(letter)
     new_index = letter_index - password_shift
+    if new_index >= len(letter_list):
+        new_index -= len(letter_list)
     encrypted_word += letter_list[new_index]
 
-print(encrypted_word)
+print(f"Your word is {encrypted_word}")
